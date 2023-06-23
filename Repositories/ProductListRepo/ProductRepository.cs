0212703fd8b3297400716ae4872bdd4a321fe79e
Mateusz Nosel <mateusz.nosel@microsoft.wsei.edu.pl>
2023-06-23 13:11:40 +0200

    product repo done

--- Repositories/ProductListRepo/ProductRepository.cs
+++ Repositories/ProductListRepo/ProductRepository.cs
@@ -1,6 +1,8 @@
-﻿using MyShopingList.Models;
+﻿using MyShopingList.Database;
+using MyShopingList.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,29 +11,43 @@ namespace MyShopingList.Repositories.ProductListRepo
 {
     public class ProductRepository : IProductRepository
     {
+        private readonly ShoppingDbContext _dbContext;
+
+        public ProductRepository(ShoppingDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
         public void AddProduct(Product product)
         {
-            throw new NotImplementedException();
+            _dbContext.Products.Add(product);
+            _dbContext.SaveChanges();
         }
 
         public void DeleteProduct(int productId)
         {
-            throw new NotImplementedException();
+            var product = _dbContext.Products.Find(productId);
+            if (product != null)
+            {
+                _dbContext.Products.Remove(product);
+                _dbContext.SaveChanges();
+            }
         }
 
         public List<Product> GetAllProducts()
         {
-            throw new NotImplementedException();
+            return _dbContext.Products.ToList();
         }
 
         public Product GetProductById(int productId)
         {
-            throw new NotImplementedException();
+            return _dbContext.Products.Find(productId);
         }
 
         public void UpdateProduct(Product product)
         {
-            throw new NotImplementedException();
+            _dbContext.Products.Attach(product);
+            _dbContext.Entry(product).State = EntityState.Modified;
+            _dbContext.SaveChanges();
         }
     }
 }
